875622229999f3e72a9a41d62e5610de59c4a062
mbalharia55 <mrunalbalharia7796@gmail.com>
2022-06-05 11:27:44 +0530

    branch 'issue1' first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 
 public class Main {
+	public static Greet() {
+		System.out.println("Good morning");
+	}
 
 	public static void main(String[] args) {
 		System.out.print("This is the main class");
